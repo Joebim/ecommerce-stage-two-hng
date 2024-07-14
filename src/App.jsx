33fc8d1320139362c105172943477be35e1b8c45
= <=>
2024-07-14 01:18:32 +0100

    [ADD] fixes to icon and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+import { lazy, Suspense } from "react";
 import { Route } from "react-router-dom"
 import { Routes } from "react-router-dom"
-import Home from "./pages/Home.jsx"
-import ProductView from "./pages/ProductView.jsx"
-import Navbar from "./components/Navbar.jsx"
-import Footer from "./components/Footer.jsx"
-import Cart from "./pages/Cart.jsx"
-import Checkout from "./pages/Checkout.jsx"
-import OrderConfirm from "./pages/OrderConfirm.jsx"
+const Home = lazy(() => import('./pages/Home.jsx'));
+const ProductView = lazy(() => import('./pages/ProductView.jsx'));
+const Navbar = lazy(() => import('./components/Navbar.jsx'));
+const Footer = lazy(() => import('./components/Footer.jsx'));
+const Cart = lazy(() => import('./pages/Cart.jsx'));
+const Checkout = lazy(() => import('./pages/Checkout.jsx'));
+const OrderConfirm = lazy(() => import('./pages/OrderConfirm.jsx'));
 import './App.css'
 import { ContextProvider } from "./api/Context.jsx"
+import scrollToTop from "./components/scrollToTop.js";
 
 
 function App() {
@@ -16,15 +18,17 @@ function App() {
   return (
     <>
       <ContextProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/order-confirm" element={<OrderConfirm />} />
-          <Route path="/product-view/:id" element={<ProductView />} />
-        </Routes>
-        <Footer />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/order-confirm" element={<OrderConfirm />} />
+            <Route path="/product-view/:id" element={<ProductView />} />
+          </Routes>
+          <Footer />
+        </Suspense>
       </ContextProvider>
 
     </>
